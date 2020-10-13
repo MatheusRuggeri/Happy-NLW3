8bed301f3163f2e6b597dd3d0a662f56c904449c
MatheusRuggeri <jomarumu@yahoo.com.br>
2020-10-13 16:33:46 -0300

    First migration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,29 +1,12 @@
 import express, { response } from 'express';
 
+import './database/connection'
+
 const app = express();
 
 app.use(express.json());
 
-// Rota = conjunto
-// Recurso = usuário
-
-// Método HTTP = GET, POST, PUT, DELETE
-// Parâmetros 
-
-// GET = Buscar informação
-// POST = Criar
-
-// PUT = Editar
-// DELETE = Deletar
-
-// Query Params: http://localhost:3333/users?search=joao
-// Route Params: http://localhost:3333/users/1 (Identificar um recurso)
-// Body: http://localhost:3333/users/ 
-
 app.get('/users', (request, response) => {
-    console.log(request.query)
-    console.log(request.params)
-    console.log(request.body)
     return response.json({ message: 'Hello World'});
 })
 
